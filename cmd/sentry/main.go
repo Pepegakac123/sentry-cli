8bc2ce07fc3b44ff4335e7342a93621659bfd86e
Kacper Adamczyk <kacpperr.adamczyk@gmail.com>
2025-12-24 18:11:04 +0100

    Implement the func that read CPU usage from the proc/stat file

--- cmd/sentry/main.go
+++ cmd/sentry/main.go
@@ -14,12 +14,18 @@ func main() {
 
 	ticker := time.Tick(*interval)
 	for range ticker {
-		usage, err := monitor.GetRamUsage()
+		ramUsage, err := monitor.GetRamUsage()
 		if err != nil {
 			fmt.Printf("Failed to get memory usage: %v\n", err)
 			return
 		}
-		fmt.Printf("Total Memory: %d\t Available Memory: %d\t Memory Usage: %.2f%%\n", usage.TotalMemory, usage.AvailableMemory, usage.MemoryUsage)
+		fmt.Printf("Total Memory: %d\t Available Memory: %d\t Memory Usage: %.2f%%\n", ramUsage.TotalMemory, ramUsage.AvailableMemory, ramUsage.MemoryUsage)
+		cpuUsage, err := monitor.GetCpuUsage()
+		if err != nil {
+			fmt.Printf("Failed to get CPU usage: %v\n", err)
+			return
+		}
+		fmt.Printf("CPU Usage: %.2f%%\n", cpuUsage)
 	}
 
 }
